1d3b1b3ed81998ee00f26c6757e7c2c2bd2912db
dDevilbiss <ddevilbiss@gmail.com>
2026-07-31 01:30:57 -0400

    First working version
    many issues - failing at write somnogram now

--- PreClinicalEEGFramework/NSB_SleepScoring.m
+++ PreClinicalEEGFramework/NSB_SleepScoring.m
@@ -661,12 +661,19 @@ switch upper(ScoringType)
         
         % generate logarithmic binning
         X = floor(logspace(0,log10(50),10)) +1;
+        %   [0,2,3,4,6,9,14,21, 33,50]
+        %X = floor(logspace(F(1),log10(50),10));
+        %forced divisions
+        %X = [0,2,3,4,5,7,12,16,30,50];
+
+        Xoffset = [0,-1,0,0.5,0.5,-0.5,-1.5, -3, -3, 0];
+        X = X + Xoffset;
         X(1) = F(1);
         counter = 1;
         for n = 1:length(X)-1
             fStart = find(F == X(n),1);
             fEnd = find(F == X(n+1),1);
-            rebinPSD(counter,:) = mean(smP(fStart:fEnd,:),1);
+            rebinPSD(counter,:) = mean(smP(fStart:fEnd,:),1); %average smooth power (i.e. Bin 1 = 0.5-2 hz) 
             rebinFreq(counter) = F(fStart);
             counter = counter +1;
         end
@@ -687,7 +694,7 @@ switch upper(ScoringType)
         TotalNumClusters = 5;
         ChiCDF = [];
         BIC=ones(1,TotalNumClusters) * NaN;
-        if options.Scoring.useGMMinit
+        if options.Scoring.useGMMinit %< Option 1 using initialization parameters for 5 states
             try
                 minnumComponents = length(options.Scoring.GMMinit.PComponents);
                 obj = gmdistribution.fit(rebinPSD(validBins,:), minnumComponents, 'Start', options.Scoring.GMMinit);
@@ -703,7 +710,7 @@ switch upper(ScoringType)
                 end
             end
         else
-            if options.Scoring.force5GMMclusters
+            if options.Scoring.force5GMMclusters %< Option 2 force 5 states w/o initial starting point
                 try
                     minnumComponents = 5;
                     obj{minnumComponents} = gmdistribution.fit(rebinPSD(validBins,:), 5, 'Start', 'randSample', 'Replicates', 5);
@@ -718,17 +725,17 @@ switch upper(ScoringType)
                 end
                 
             else
-                for n=1:TotalNumClusters
+                for n=1:TotalNumClusters %< Option 3 iterate and find the number of states w/o initial starting point
                     disp(['... Running GMM Cluster Size: ',num2str(n)]);
                     try
                         obj{n} = gmdistribution.fit(rebinPSD(validBins,:), n, 'Start', 'randSample', 'Replicates', 5);
                         BIC(n) = obj{n}.BIC;
                         if n == 2
-                            %do deviance test to see if truely diferent
+                            %do deviance test to see if 1st 2 clusters are truely diferent
                             difdev = abs(obj{n}.NlogL - obj{n-1}.NlogL);
                             df = n - (n-1);
                             ChiCDF = 1 - chi2cdf(difdev, df);
-                            if ChiCDF >= 0.01 | max(rebinPSD(validBins,3)) <= 2* mean(rebinPSD(validBins,3)) % << soft code this !!
+                            if ChiCDF >= 0.01 || max(rebinPSD(validBins,3)) <= 2* mean(rebinPSD(validBins,3)) % << soft code this !!
                                 % no Wake and Sleep
                                 break;
                             end
@@ -745,11 +752,17 @@ switch upper(ScoringType)
                     end
                 end
             end
+
             if ~isempty(ChiCDF)
                 [minBIC,minnumComponents] = min(BIC);
-                if minnumComponents > 5
+                disp(['Final GMM Cluster Size = ',num2str(minnumComponents)]);
+                if minnumComponents >= 5         % <<<< finds break point in BIC curve (Scree test) to choose the number of states when over 5 states
                     for j=2:TotalNumClusters
-                        ypredict=detrend(BIC(2:10),'linear',[j-1]);
+                        if length(BIC) >= 10
+                            ypredict=detrend(BIC(2:10),'linear',[j-1]);
+                        else
+                            ypredict=detrend(BIC(2:end),'linear',[j-1]);
+                        end
                         SumSquare(j-1)=sum(ypredict.^2);
                     end
                     [MinSS,breakPt]=min(SumSquare);
@@ -762,47 +775,176 @@ switch upper(ScoringType)
             end
             warning on;
             disp(['Final Number of Clusters = ',num2str(minnumComponents)]);
-            [idx,nlogl,NormSpectralMatrix] = cluster(obj{minnumComponents},rebinPSD);
+            try
+                [idx,nlogl,NormSpectralMatrix] = cluster(obj{minnumComponents},rebinPSD); %n=1 obj unrecognized
+            catch ME
+                errorstr = ['Error: NSB_SleepScoring >> GMM cannot generate even 1 Cluster - Failed ',ME.message];
+                if ~isempty(options.LogFile)
+                    NSBlog(options.LogFile,errorstr);
+                else
+                    errordlg(errorstr,'NSB_SleepScoring');
+                end
+                %idx = zeros(1,size(rebinPSD,1));
+                return; %Scoring a dead channel has issues and throws errors
+            end
         end
         
+        %ToDo: incorporate probabilities
+
         %Now identify clusters
         % 1st Gather spectra means
-        for n = 1:5
+        for n = 1:minnumComponents
             meanSpectra(n,:) = mean(rebinPSD(idx == n,:));
         end
-        %2nd apply simple classification scheme related to scoring order
-        [trash, rowIdx] = sortrows(meanSpectra,[-2 -3]); %sort in decending order by delta power
-        isNaN_idx = isnan(trash(:,1));
-        if any(isNaN_idx)
-            if sum(isNaN_idx) == 3 %2 states
-                scoringSet = [2,4];
-            elseif sum(isNaN_idx) == 2
-                scoringSet = [2,3,4];
-            else
-                scoringSet = [2,3,4,5];
-            end
+        % identify empty clusters and remove
+        isNaN_idx = isnan(meanSpectra(:,1));
+        meanSpectra = meanSpectra(~isNaN_idx,:);
+
+        % 2nd apply simple classification (labeling) scheme
+        % assign each cluster an internal state value to be used by archetecture rules and to combine epochs
+        % rowIdx = [N3idx,N1idx,QWidx,Widx,Ridx]; %orders the clusters by state
+        % scoringSet = [2,3,4,5,1]; %scoring order sws2, sws1, wake, aw, ps
+
+        StateLookup = cell(6,3); % State name; scoringSet; cluster number
+        StateLookup(:,1) = {'SWS2'; 'SWS1'; 'QW'; 'AW'; 'PS'; 'UNK'};
+        StateLookup(:,2) = {2;3;4;5;1;0};
+        StateLookup(:,3) = num2cell(ones(1,6)*NaN);
+
+        % AASM 2026 v3
+        %[N3idx,N1idx,QWidx,Widx,Ridx] = deal(NaN);
+        %N3 0.5-2Hz that are > 20% of the 30 sec epoch
+        [DeltaSort, N3idx] = sortrows(meanSpectra,[-2 -3],'MissingPlacement','last'); %N3 sort in decending order by delta power
+        if sum(DeltaSort(N3idx(1),2:3),2) > sum(DeltaSort(N3idx(1),5:6),2)
+            %Then it really is SWS2/N3
+            %N3idx = N3idx(1);
+            StateLookup{1,3} = N3idx(1);
         else
-        scoringSet = [2,3,4,5,1]; %scoring order
+            LogStr = ['Warning: NSB_SleepScoring >> GMM Cannot find N3/SWS'];
+            disp(LogStr);
+            NSBlog(options.LogFile,LogStr);
         end
-        rowIdx(isNaN_idx,:) = [];
-        trash(isNaN_idx,:) = [];
+
+        % Rem Sawtooth waves 2-6 Hz,LAMF w/o spindles or K-complexes
+        [~, Ridx] =sortrows(sum(meanSpectra(:,4:5),2),[-1],'MissingPlacement','last'); %R sort in decending order by Theta power (4.5-8.5Hz)
+        %Ridx = Ridx(1);
+        StateLookup{5,3} = Ridx(1);
+
+        %N1 LAMF 4-7 Hz > 50% of the epoch + more power in slow Hz
+        %N2 K-complex, Sleep Spindle 11-16 Hz > 50% of the epoch
+        % Waking alpha activity 8-13 Hz
+        [~, rowIdx] = sortrows(meanSpectra,[-4],'MissingPlacement','last'); %decending order by low Theta power (4.5-6.5Hz)
+        rowIdx(find(rowIdx==StateLookup{1,3} | rowIdx==StateLookup{5,3})) = [];
+
+        if sum(~isNaN_idx) == 5 %All 5 states exist
+
+            %N1 LAMF 4-7 Hz > 50% of the epoch + more power in slow Hz
+            %Delta 2+3 / Beta 6+7 (Ratio)
+            [~, N12idx] =sortrows(sum(meanSpectra(rowIdx,2:3),2)./sum(meanSpectra(rowIdx,6:7),2),[-1],'MissingPlacement','last'); %N1 sort in decending order by delta/beta power
+            %N1idx = rowIdx(N12idx(1));
+            StateLookup{2,3} = rowIdx(N12idx(1));
+
+            % Waking alpha activity 8-13 Hz (with Theta)
+            %Widx = rowIdx(N12idx(end));
+            StateLookup{4,3} = rowIdx(N12idx(end));
+
+            %Quiet waking
+            QWidx = 1:n;
+            %QWidx([Widx,N1idx,N3idx,Ridx]) = [];
+            temp = cell2mat(StateLookup(:,3));  temp(isnan(temp))=[];
+            QWidx(temp) = [];
+            StateLookup{3,3} = QWidx;
+
+        elseif sum(~isNaN_idx) == 4 % Only 4 states exist
+            %N1 LAMF 4-7 Hz > 50% of the epoch + more power in slow Hz
+            %Delta 2+3 / Beta 6+7 (Ratio)
+            [~, N12idx] =sortrows(sum(meanSpectra(rowIdx,2:3),2)./sum(meanSpectra(rowIdx,6:7),2),[-1],'MissingPlacement','last'); %N1 sort in decending order by delta/beta power
+            %N1idx = rowIdx(N12idx(1));
+            StateLookup{2,3} = rowIdx(N12idx(1));
+
+            % Waking alpha activity 8-13 Hz (with Theta)
+            %Widx = rowIdx(N12idx(end));
+            StateLookup{4,3} = rowIdx(N12idx(end));
+
+            LogStr = ['Warning: NSB_SleepScoring >> GMM only found 4 states'];
+            disp(LogStr);
+            NSBlog(options.LogFile,LogStr);
+
+        elseif sum(~isNaN_idx) == 3 % Only 3 states exist
+            %Final states are SWS2, AW/PS, QW/N1
+            %QWidx = rowIdx;
+            StateLookup{3,3} = rowIdx;
+
+            LogStr = ['Warning: NSB_SleepScoring >> GMM only found 3 states'];
+            disp(LogStr);
+            NSBlog(options.LogFile,LogStr);
+        else
+            LogStr = ['Warning: NSB_SleepScoring >> GMM only found 2 states'];
+            disp(LogStr);
+            NSBlog(options.LogFile,LogStr);
+        end
+
+        % rowIdx = [N3idx,N1idx,QWidx,Widx,Ridx]; %orders the clusters by state
+        % scoringSet = [2,3,4,5,1]; %scoring order sws2, sws1, wake, aw, ps
+        % 
+        % %old
+        % %[DeltaSort, rowIdx] = sortrows(meanSpectra,[-2 -3]); %sort in decending order by delta power
+        % %isNaN_idx = isnan(DeltaSort(:,1));
+        % 
+        % switch minnumComponents
+        %     case 1
+        %         if ~isnan(N3idx)
+        %             scoringSet = [2];
+        %         elseif ~isnan(Ridx)
+        %             scoringSet = [4];
+        %         else
+        %             scoringSet = [0];
+        %         end
+        %     case 2
+        %         scoringSet = [2,4];
+        %     case 3
+        %         scoringSet = [2,3,5];
+        %     case 4
+        %         scoringSet = [2,3,4,5];
+        %     case 5
+        %         scoringSet = [2,3,4,5,1]; %scoring order sws2, sws1, wake, aw, ps
+        %     otherwise
+        %         scoringSet = [2,3,4,5,1]; %scoring order sws2, sws1, wake, aw, ps
+        % end
+        % 
+        % %rowIdx(isNaN_idx,:) = [];
+        % %DeltaSort(isNaN_idx,:) = [];
         
         % plot if requested
         if options.Scoring.plot
             fh = figure;
-            ph = plot(trash');
-            CurAxis = get(fh,'Children');
-            title(CurAxis,'GMMlogSpectrum Cluster Profiles');
-            switch length(scoringSet)
+            switch sum(~isNaN_idx)
+                case 1
+                   ph = plot(meanSpectra');
+                   CurAxis = get(fh,'Children');
+                   if ~isnan(StateLookup{1,3})
+                    legend(CurAxis, 'SWS2');
+                   else
+                   legend(CurAxis, 'QW/AW/PS'); 
+                   end
                 case 2
-                   legend(CurAxis,'SWS2','QW'); 
+                   ph = plot(meanSpectra([StateLookup{1,3},StateLookup{5,3}],:)');
+                   CurAxis = get(fh,'Children');
+                   legend(CurAxis,'SWS2','QW/AW/PS'); 
                 case 3
-                   legend(CurAxis,'SWS2','SWS1','QW'); 
+                    ph = plot(meanSpectra([StateLookup{1,3},StateLookup{3,3},StateLookup{5,3}],:)');
+                    CurAxis = get(fh,'Children');
+                   legend(CurAxis,'SWS2','QW/SWS1','AW/PS'); 
                 case 4
-                   legend(CurAxis,'SWS2','SWS1','QW','AW'); 
+                    ph = plot(meanSpectra([StateLookup{1,3},StateLookup{2,3},StateLookup{4,3},StateLookup{5,3}],:)');
+                    CurAxis = get(fh,'Children');
+                   legend(CurAxis,'SWS2','SWS1','AW','PS'); 
                 otherwise
+                    ph = plot(meanSpectra(cell2mat(StateLookup{:,3}),:)');
+                    CurAxis = get(fh,'Children');
                     legend(CurAxis,'SWS2','SWS1','QW','AW','PS');
             end
+            title(CurAxis,'GMMlogSpectrum Cluster Profiles');
+
             if options.MatlabPost2014
                 xlabel(CurAxis,'Frequency (Hz)');
                 ylabel(CurAxis,'Normalized Power');
@@ -822,14 +964,15 @@ switch upper(ScoringType)
             close(fh);
         end
         
-        %assign scoring
-        ScoreIndex = zeros(1,size(rebinPSD,1)); %pallette is the length/binned the same as FFT (i.e. 5sec)
-        for n = 1:length(scoringSet)
-            if ~isempty(intersect(rowIdx,n))
-                ScoreIndex(idx == rowIdx(n)) = scoringSet(n);
+        %assign scoring to clusters
+        ScoreIndex = zeros(1,size(rebinPSD,1)); %scoring pallette is the length/binned the same as FFT (i.e. 5sec)
+        for n = 1:size(StateLookup,1)
+            % 
+            if any(cell2mat(StateLookup(:,3)) == n) % did we assign a state to cluster n?
+                ScoreIndex(idx == n) = StateLookup{cell2mat(StateLookup(:,3)) == n,2}; %find cluster in StateLookup and return the scoringSet and assign to ScoreIndex
             end
         end
-        
+          
         %debug
         %save testFile
         
@@ -845,7 +988,6 @@ switch upper(ScoringType)
         end
         return;
 end
-
 
 % %Archtecture Rules
 if options.rules.ApplyArchitectureRules
